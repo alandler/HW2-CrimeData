e8293daf5e5737e65b8a0dc90a6a5c4be411a51f
alandler <alandler@mit.edu>
2019-02-26 15:27:47 -0500

    Exercise updated

--- exercise.js
+++ exercise.js
@@ -1,37 +1,40 @@
 var exercise = {};
 
 exercise.countRecords = function(data){
-    //-------------------------------------------
-    // YOUR CODE
-    // Return the total number of records
-    //-------------------------------------------
+    return data.length;
     return 'Error: countRecords not implemented'; 
 };
 
 exercise.countDistrictCrimes = function(data,district){
-    //-------------------------------------------
-    // YOUR CODE
-    // Return the number of crimes 
-    // for the given district
-    //-------------------------------------------
+    counter = 0
+    for (var i = 0; i<data.length; i++){
+    if (data[i][19] == district) {
+        counter += 1
+    }
+}
+    return counter
     return 'Error: countDistrictCrimes not implemented'; 
 };
 
 exercise.countPrimaryType = function(data,primaryType){
-    //-------------------------------------------
-    // YOUR CODE
-    // Return the number of crimes 
-    // for the giving primary type
-    //-------------------------------------------
+    counter = 0
+    for (var i = 0; i<data.length; i++){
+    if (data[i][13] == primaryType) {
+        counter += 1
+    }
+}
+    return counter
     return 'Error: countPrimaryType not implemented';
 };
 
 exercise.countLocation = function(data,location){
-    //-------------------------------------------
-    // YOUR CODE
-    // Return the number of crimes 
-    // for the given location
-    //-------------------------------------------
+    counter = 0
+    for (var i = 0; i<data.length; i++){
+    if (data[i][15] == location) {
+        counter += 1
+    }
+}
+    return counter
     return 'Error: countLocation not implemented';
 };
 
@@ -48,6 +51,12 @@ exercise.buildLatLngPoint = function(crime){
     // point.longitude = crime[longitudeColumnNumber];
     // return point;    
     //-------------------------------------------    
+    var point = {};    
+    point.latitude = crime[26];
+    point.longitude = crime[28];
+    return point;   
+
+
     return 'Error: buildLatLngPoint not implemented';
 };
 
